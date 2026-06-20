9c20290b3e5347089063d976e68cf82ca26e2507
VisheshSandeshShetty <visheshshetty33@gmail.com>
2026-06-20 09:16:31 +0000

    Etch-A-Sketch implemented in a 512 grid with clear screen function

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # etch-a-sketch
 
 16x16 grid constructed to provide an etch-a-sketch screen.
+
+Drawing when mouse is over the sketch screen is done along with a clear option to restart the screen completely and redo the drawning from scratch.
--- javascript.js
+++ javascript.js
@@ -1,6 +1,20 @@
+const container=document.querySelector('#container');
 const screen=document.querySelector('.sketch-screen');
+
 for(let i=0;i<256;i++){
     const pad=document.createElement('div');
     pad.classList.add("pad");
     screen.appendChild(pad);
+    
+    pad.addEventListener('mouseover', ()=>{
+        pad.style.backgroundColor='black';
+    })
 }
+
+const clear=document.querySelector('#clear');
+    clear.addEventListener('click', ()=>{
+        const pads=document.querySelectorAll('.pad');
+        pads.forEach((pad)=>{
+            pad.style.backgroundColor='white';
+        });
+    })
